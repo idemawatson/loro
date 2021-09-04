f3e52ce5260eb87e5cb884e715a27a7d2e5bf008
idetakumi <boc.10194@gmail.com>
2021-09-04 23:11:36 +0900

    refactor_20210904

--- server/loro-handler/database/database.go
+++ server/loro-handler/database/database.go
@@ -11,7 +11,8 @@ import (
 func GetTable(tableName string) *dynamoClient {
 	cfg := aws.NewConfig().WithRegion(config.GetEnv("REGION"))
 	db := dynamo.New(session.Must(session.NewSession()), cfg)
-	return &dynamoClient{table: db.Table(config.GetEnv(tableName))}
+	table := db.Table(config.GetEnv(tableName))
+	return &dynamoClient{table: table}
 }
 
 type dynamoClient struct {
